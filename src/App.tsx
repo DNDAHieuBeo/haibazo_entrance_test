73cd4ecc0762d2cb377cb4b3ee5f8400b7dd8a48
Hieu Le <mac@Hieu-Le.local>
2025-05-07 13:39:31 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+   <div className="h-screen w-full items-center justify-center bg-white">
+        <div className='w-16 bg-white border border-black-600 '>
+            hello
+        </div>
+   </div>
+  )
+}
+
+export default App
